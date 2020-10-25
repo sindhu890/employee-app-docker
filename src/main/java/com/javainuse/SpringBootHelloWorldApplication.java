1bad7a9ad767da1a9b06d34f849850db6db155da
Sindhu <SIKR0000@skopnwtispap01.ESS.pnad.pninfrasrv.net>
2020-10-25 22:26:06 +0100

    package chagnes

--- src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
+++ src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
@@ -6,11 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.javainuse.controller.EmployeeController;
-
 @EnableAutoConfiguration
 @SpringBootApplication
-@ComponentScan(basePackageClasses=EmployeeController.class)
+@ComponentScan()
 public class SpringBootHelloWorldApplication {
 
 	public static void main(String[] args) {
