a115b3ebb4591344bae3ec8ad2eb2072f285d146
Sindhu <SIKR0000@skopnwtispap01.ESS.pnad.pninfrasrv.net>
2020-10-25 22:20:05 +0100

    annotation class

--- src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
+++ src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
@@ -3,10 +3,14 @@ package com.javainuse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.javainuse.controller.EmployeeController;
 
 @EnableAutoConfiguration
 @SpringBootApplication
+@ComponentScan(basePackageClasses=EmployeeController.class)
 public class SpringBootHelloWorldApplication {
 
 	public static void main(String[] args) {
